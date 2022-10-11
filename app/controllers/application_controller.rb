5b7489cadb7b2a4e0aeb830520c778b7a7ccfdde
EC2 Default User <ec2-user@ip-172-31-33-75.ec2.internal>
2022-10-11 17:53:56 +0000

    first

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top,:about]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resorce)
+
+      user_path(current_user.id) #Sign up
+  end
+  def after_sign_out_path_for(resorce)
+    '/'
+  end
+
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:email])
+  end
+
+end
---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,71 @@
+class BooksController < ApplicationController
+before_action :correct_book, only: [:edit, :update]
+
+  def new
+    @book=Book.new
+
+  end
+  def create
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+       redirect_to book_path(@book.id)
+    else
+       @books=Book.all
+
+      render :index
+    end
+
+  end
+
+  def index
+    @book=Book.new
+    @books=Book.all
+  end
+
+  def show
+    @book=Book.find(params[:id])
+    @user=current_user
+  end
+
+  def edit
+    @book=Book.find(params[:id])
+
+  end
+  def update
+    @book=Book.find(params[:id])
+    if  @book.update(book_params)
+       flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+
+      render :edit
+    end
+
+
+  end
+
+  def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def correct_book
+    @book = Book.find(params[:id])
+    unless @book.user.id==current_user.id
+      redirect_to books_path
+    end
+
+  end
+
+
+end
