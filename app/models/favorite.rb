6f8a5c1d76b8b91ab381f769e8ea1c5bd023e7f1
EC2 Default User <ec2-user@ip-172-31-33-75.ec2.internal>
2022-10-18 11:44:01 +0000

    no-error

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,6 +2,6 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  validates :user_id, uniqueness: {scope: :post_id}
+  validates :user_id, uniqueness: {scope: :book_id}
 
 end
